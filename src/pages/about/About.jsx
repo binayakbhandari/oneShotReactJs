93e1f144c9b0378142dbc858f1303a097ea0b7dd
binayakbhandari <binayakbhandari60@gmail.com>
2024-09-06 23:25:56 +0545

    create button

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react"
 import Button from "../../components/Button"
 
 function About(){
+    const name = "Binayak"
+    var count = 0
+    console.log(useState())
+    // const firstItem = useState()[0]
+    // const secondItem = useState()[0]
+    // const [firstItem, secondItem] = useState(100)
+    // const [number, setNumber] = useState(0)
+    const state = useState({})
+    const number = state()[0]
+    const setNumber = state(0)[1]
+    
+    const increase = ()=>{
+        setNumber(number+1)
+    }
+    const decrease = ()=>{
+        setNumber(number-1)
+    }
     return(
         <>
-            <div style={{display:'flex'}}>
+            <div>
                 <h1>Welcome to About page</h1>
                 <Button type="Learn More" />
+
+                <h1>{number}</h1>
+                <button onClick={decrease}>-</button>
+                <button onClick={increase}>+</button>
             </div>
         </>
     )
